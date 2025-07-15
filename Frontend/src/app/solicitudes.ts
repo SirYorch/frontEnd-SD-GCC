c838f8d882ee150880d4be3d1d2c842f2053ba6b
SirYorch <jcuevam3@est.ups.edu.ec>
2025-07-15 17:29:34 -0500

    Actualizacion frontend para balanceo de carga, detección de nodos caidos, y reintentos automáticos y rimeouts

--- Frontend/src/app/solicitudes.ts
+++ Frontend/src/app/solicitudes.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { retry, timeout, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +11,30 @@ export class Solicitudes {
 
   private readonly BACKEND_URL = '/api';
 
+  private readonly TIMEOUT_MS = 5000; // 5 segundos
+  private readonly MAX_RETRIES = 7;
+
   constructor() { }
-  /**
-   * Recupera todos los posts desde el backend (5001)
-   */
+
   getSolicitudes(): Observable<any> {
-    return this.http.get(`${this.BACKEND_URL}/datos`);
+    return this.http.get(`${this.BACKEND_URL}/datos`).pipe(
+      timeout(this.TIMEOUT_MS),  //tiempo de espera que colocamos
+      retry(this.MAX_RETRIES), //cantidad de intentos a una solicitud
+      catchError((error) => {
+        console.error('Error en getSolicitudes:', error);
+        return throwError(() => error);
+      })
+    );
   }
 
-  /**
-   * Envía texto + nombre de imagen al backend (5001)
-   */
   EnviarCorreo(dato: any): Observable<any> {
-    return this.http.post(`${this.BACKEND_URL}/insertar`, dato);
+    return this.http.post(`${this.BACKEND_URL}/insertar`, dato).pipe(
+      timeout(this.TIMEOUT_MS),
+      retry(this.MAX_RETRIES),
+      catchError((error) => {
+        console.error('Error en EnviarCorreo:', error);
+        return throwError(() => error);
+      })
+    );
   }
 }
